88de95d5c8f8f30ffbf67d036bfed878d5c6d0dc
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-27 17:33:48 -0700

    on last exercise up to style edit

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -17,9 +17,6 @@ function showFortune(evt) {
 $('#get-fortune-button').on('click', showFortune);
 
 
-
-
-
 // PART 2: SHOW WEATHER
 function weather(result) {
     $('#weather-info').html(result.forecast);
@@ -27,28 +24,35 @@ function weather(result) {
 
 function showWeather(evt) {
     evt.preventDefault();
+    // TODO: request weather with that URL and show the forecast in #weather-info
 
     let url = "/weather.json";
-    let formData = {"zipcode": $("#zipcode-field").val()};
+    let formData = {"zipcode": $("#zipcode-field").val(),};
 
     $.get(url, formData, weather);
-
-
-    // TODO: request weather with that URL and show the forecast in #weather-info
+    // $.get(url, formData, function(result) {$('#weather-info').html(result.forecast);}
 }
 
 $("#weather-form").on('submit', showWeather);
-
-
 
 
 // PART 3: ORDER MELONS
 
 function orderMelons(evt) {
     evt.preventDefault();
-
     // TODO: show the result message after your form
+
+    let url = "/order-melons.json";
+    let formData = {"melon-type": $("#melon-type-field").val(),
+                    "qty": $("qty-field").val(),};
+
+    $.post(url, formData, function(result) {$("#order-status").html(result.msg);});
+
     // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+    if (result.code === "ERROR") {
+        let text = $("#order-error");
+        text.css("color", "red");
+    }
 }
 
 $("#order-form").on('submit', orderMelons);
